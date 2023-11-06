9b48b787e2f5c30c90c8572405f0296335ac4c11
AydenWells.github.io <you@example.com>
2023-11-05 22:10:25 -0500

    updated

--- server.js
+++ server.js
@@ -13,42 +13,42 @@ app.get("/api/members", (req, res) => {
     title:"Daughter of James and Jessica",
     currentAge:"19",
     birthday:"November 17th, 2003", 
-    img:"images/download.jpg",
+    img:"ayden.jpg",
 };
 members [1]={
     name:"Jessica Rose Matthews",
     title:"Mom of Tristin, Ayden, and Riley, & Married to Aubrey, & Ex-wife to James",
     currentAge:"44",
     birthday:"November 29th, 1978", 
-    img:"images/jessica.jpg",
+    img:"jessica.jpg",
 };
 members [2]={
     name:"James Downing Mathews III",
     title:"Dad of Tristin, and Ayden & Ex-husband to Jessica",
     currentAge:"45",
     birthday:"March 17th, 1978", 
-    img:"images/james.jpg",
+    img:"james.jpg",
 };
 members [3]={
     name:"Aubrey Adam Matthews",
     title:"Step-dad of Tristin, and Ayden, Dad of Riley, & Husband to Jessica",
     currentAge:"45",
     birthday:"June 23rd, 1978", 
-    img:"images/aubrey.jpg",
+    img:"aubrey.jpg",
 };
 members [4]={
     name:"Tristin James Bradley Mathews",
     title:"Son of James and Jessica",
     currentAge:"24",
     birthday:"December 18th, 1998", 
-    img:"images/tristin.jpg",
+    img:"tristin.jpg",
 };
 members [5]={
     name:"Riley Adam Matthews",
     title:"Son of Aubrey and Jessica",
     currentAge:"14",
     birthday:"January 4th, 2009", 
-    img:"images/riley.jpg",
+    img:"riley.jpg",
 };
     res.json(members);
 });
